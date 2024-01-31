4176bc44c59d39414374b0d51ba259934960330b
elifxa <elif.akcay@std.antalya.edu.tr>
2024-01-31 10:50:39 +0300

    first commit

---
+++ src/components/Content.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodoList from './TodoList';
+import ContentFooter from './ContentFooter';
+
+function Content() {
+  return (
+    <div>
+      <section className="main">
+        <input className="toggle-all" type="checkbox" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <TodoList />
+      </section>
+
+      <ContentFooter />
+    </div>
+  );
+}
+
+export default Content;
